5462d1138ec37f001a6cc6817d9f2b62fd278c47
Kostya Cholak <kostya.cholak@gmail.com>
2023-02-25 15:15:02 +0200

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name="agci",
     version="1.0",
-    packages=find_packages(),
+    packages=['agci'],
     install_requires=[],
 )
